b835f5ebac2b9bf3f6a7f7ba927af9e74b0249aa
AndriiHavryliuk1 <andriy.gavrulyik@gmail.com>
2018-07-10 19:31:58 +0300

    basic redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import {render} from "react-dom";
 
@@ -28,3 +29,41 @@ class App extends React.Component {
 }
 
 render(<App />, window.document.getElementById('app'));
+
+*/
+
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "ADD":
+            state += action.payload;
+            break;
+        case "SUBTRACT":
+            state -= action.payload;
+            break;
+    }
+
+    return state;
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.log("Store updated", store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 50
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 10
+});
